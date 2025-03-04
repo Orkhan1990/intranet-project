0b5ba749b7c3285c1b860795c3cdeb3c00fc8d88
Orkhan Talibli <orkhan.talibli@gmail.com>
2025-03-04 22:14:29 +0400

    04.03.25

--- backend/src/controllers/orderPartController.ts
+++ backend/src/controllers/orderPartController.ts
@@ -9,8 +9,19 @@ const orderRepository=AppDataSource.getRepository("Order");
 export const getOrderPartsByOrderId = async (req: Request, res: Response,next:NextFunction) => {
     try {
 
-        const{ id } = req.params;
-        const orderParts=await orderPartRepository.find({where:{order:id}});
+        const{id} = req.params;
+        console.log(id);
+
+        // const order=await orderRepository.findOneBy({id});
+        // // console.log(order);
+        // if(!order){
+        //     return next(errorHandler(404,"Sifariş tapılmadı"));
+        // }        
+        
+        const orderParts = await orderPartRepository.find({
+            where: { order: { id: parseInt(id) } }, // Filter by orderId
+        });
+        console.log(orderParts);
         if(orderParts.length===0){
             return next(errorHandler(404,"Ehtiyyat hissələri tapılmadı"));
         }
--- frontend/src/pages/Calculation/Calculation.tsx
+++ frontend/src/pages/Calculation/Calculation.tsx
@@ -1,9 +1,10 @@
-import { Button } from "flowbite-react";
+import { Button} from "flowbite-react";
 import { useEffect, useState } from "react";
 import { OrderPartsInterface } from "../../types";
+import { useLocation } from "react-router-dom";
 
 const Calculation = () => {
-  const[orderId,setOrderId]=useState<number>(0);
+  const[error,setError]=useState<string>("");
   const [orderParts, setOrderParts] = useState<OrderPartsInterface[]>([
     {
       id: 0,
@@ -16,10 +17,25 @@ const Calculation = () => {
   ]);
 
 
+  const location=useLocation();
+  const params=new URLSearchParams(location.search);
+  const orderId=parseInt(params.get('orderId') || '');
+  const supplierId=parseInt(params.get('supplierId')||'');
+  const liquidity=(params.get('liquidity')||'');
+  console.log(orderId,supplierId,liquidity);
+  console.log(orderParts);
+  
+  
+
   useEffect(() => {
     const fetchOrderParts = async () => {
-      const response = await fetch(`http://localhost:5173/api/v1/orderPart/getOrderPartsByOrderId/1`);
-      const data = await response.json();
+      const res = await fetch(`http://localhost:3013/api/v1/orderPart/getOrderPartsByOrderId/${orderId}`);
+      const data = await res.json();
+
+      if(!res.ok||data.success===false){
+        setError(data.message);
+        return;
+      }
       setOrderParts(data);
     };
     fetchOrderParts();
@@ -133,72 +149,86 @@ const Calculation = () => {
             <td className="px-1  font-[300] text-xs border border-dashed border-black p-2 bg-yellow-200"></td>
           </tr>
 
-          <tr className="h-7">
-            <td className="px-1  text-xs w-[15px] font-[300] border border-dashed border-black p-2 "></td>
-            <td className="px-1 font-[300] text-xs border border-dashed border-black p-2 "></td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2 ">
+          {
+            orderParts&&orderParts.map((orderPart:any,index:number)=>(
+                
+          <tr className="h-7" key={index}>
+          <td className="px-1  text-xs w-[15px] font-[400] border border-dashed border-black p-2 ">{index+1}</td>
+          <td className="px-1 font-[400] text-xs border border-dashed border-black p-2 ">{orderPart.origCode}</td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2 ">
+            <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2 ">
+            <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" value={orderPart.partName} />
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <select className="w-44 text-xs h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]">
+              <option value=""></option>
+              <option value="">MetroMan (IN-TECH)</option>
+              <option value="">3F</option>
+              <option value="">3M</option>
+              <option value="">4CR</option>
+            </select>
+          </td>
+          <td className="px-1  font-[400] text-xs border border-dashed border-black p-2">{orderPart.count}</td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <div className="flex gap-2 items-end">
               <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2 ">
+              <span className="text-black font-[400]">man</span>
+            </div>
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <div className="flex gap-2 items-end">
               <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2"></td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2"></td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
-              <div className="flex gap-2 items-end">
-                <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-                <span className="text-black font-[400]">man</span>
-              </div>
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
-              <div className="flex gap-2 items-end">
-                <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-                <span className="text-black font-[400]">man</span>
-              </div>
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
-              <div className="flex gap-2 items-end">
-                <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-                <span className="text-black font-[400]">man</span>
-              </div>
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
-              <div className="flex gap-2 items-end">
-                <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-                <span className="text-black font-[400]">man</span>
-              </div>
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
-              <div className="flex gap-2 items-end">
-                <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-                <span className="text-black font-[400]">man</span>
-              </div>
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
-              <div className="flex gap-1 items-end">
-                <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-                <span className="text-black font-[400] text-sm">%</span>
-              </div>
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
-              <div className="flex gap-2 items-end">
-                <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-                <span className="text-black font-[400]">man</span>
-              </div>
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
-              <div className="flex gap-2 items-end">
-                <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-                <span className="text-black font-[400]">man</span>
-              </div>
-            </td>
-            <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
-              <div className="flex gap-2 items-end">
-                <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
-                <span className="text-black font-[400]">man</span>
-              </div>
-            </td>
-          </tr>
+              <span className="text-black font-[400]">man</span>
+            </div>
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <div className="flex gap-2 items-end">
+              <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
+              <span className="text-black font-[400]">man</span>
+            </div>
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <div className="flex gap-2 items-end">
+              <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
+              <span className="text-black font-[400]">man</span>
+            </div>
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <div className="flex gap-2 items-end">
+              <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
+              <span className="text-black font-[400]">man</span>
+            </div>
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <div className="flex gap-1 items-end">
+              <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" value={45}/>
+              <span className="text-black font-[400] text-sm">%</span>
+            </div>
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <div className="flex gap-2 items-end">
+              <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
+              <span className="text-black font-[400]">man</span>
+            </div>
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <div className="flex gap-2 items-end">
+              <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
+              <span className="text-black font-[400]">man</span>
+            </div>
+          </td>
+          <td className="px-1  font-[300] text-xs border border-dashed border-black p-2">
+            <div className="flex gap-2 items-end">
+              <input className="w-24 h-6 border border-black rounded-sm outline-none p-1 text-black font-[400]" />
+              <span className="text-black font-[400]">man</span>
+            </div>
+          </td>
+        </tr>
+            ))
+          }
+
 
           <tr className="h-7">
             <td className="px-1  text-xs w-[15px] font-[300] border border-dashed border-black p-2 bg-custom-yellow"></td>
